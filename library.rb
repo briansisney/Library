4cd23ea1fb9ba6f8a47896c1aea27e72fda9a465
MakerSquare Student <students@themakersquare.com>
2013-11-15 10:37:19 -0600

    added a checkout method in library. added borrow method in book, added reading method in book, took out available and borrowed array in library

--- library.rb
+++ library.rb
@@ -1,8 +1,6 @@
 class Library
   def initialize
     @books = []
-    @available_books=[]
-    @borrowed_books=[]
   end
 
   def books 
@@ -10,25 +8,24 @@ class Library
 
   def list_books
     @books.each do |book|
-      puts "Title: #{book.title}, Author: #{book.author}"
+      puts "Title: #{book.title}, Author: #{book.author}, Status: #{book.borrowed ? book.borrowed.name : "Available"}"
     end
   end
 
   def borrowed_books
-    @borrowed_books
+   # list borrowed with  true
   end
 
   def available_books
-    @available_books
+   # list borrowed with false
   end
 
   def add_book(book)
     @books << book 
-    @available_books << book
   end
 
   def check_out(user, book) 
-
+    book.reading(user)
   end
 
   def check_in(book)
@@ -38,10 +35,11 @@ end
 class Borrower
   def initialize(name)
     @name = name
+    
   end
 
   def borrowed_books
-
+    # list books with status of name
   end
 
   def name
@@ -49,10 +47,11 @@ class Borrower
   end
 
   def borrowed_books_count
-
+    #count list
   end
 
   def borrowed_books_list
+
   end
 end
 
@@ -60,6 +59,7 @@ class Book
   def initialize(title, author)
     @title = title
     @author = author
+    @borrowed = false
   end
     def title
     @title
@@ -68,4 +68,11 @@ class Book
   def author
     @author
   end
+  def borrowed
+    @borrowed
+  end
+  def reading(user)
+    @borrowed = user
+  end
+  
 end
